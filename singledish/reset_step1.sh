7ccefd8e7c8dffeb94323b6d09cc637703efe4a5
heh15 <he_hao_@outlook.com>
2023-01-26 20:46:17 -0500

    Reset the singledish pipeline to start from step 1

---
+++ singledish/reset_step1.sh
@@ -0,0 +1,8 @@
+rm -rf antennae/processing_singledish_antennae/calibration/*.PM*.ms
+rm -rf antennae/processing_singledish_antennae/calibration/*.PM*.ms.1
+rm -rf antennae/processing_singledish_antennae/calibration/*.PM*.ms.2
+rm -rf antennae/processing_singledish_antennae/calibration/*.PM*.ms.3
+rm -rf antennae/processing_singledish_antennae/calibration/*.PM*.ms.4
+rm -rf antennae/processing_singledish_antennae/calibration/*.PM*.ms.5
+
+rm -rf antennae/processing_singledish_antennae/calibration/done_step*
